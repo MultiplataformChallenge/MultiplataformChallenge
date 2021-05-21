51f43410dc4e659887cda298c06296223e1c473d
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2021-05-21 15:48:28 -0300

    adding some app colors

--- Multiplataform/Multiplataform/Utils/ColorExtension.swift
+++ Multiplataform/Multiplataform/Utils/ColorExtension.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension Color {
+    static let darkPurple = Color.init(.sRGB, red: 86/255, green: 11/255, blue: 173/255, opacity: 1)
+    static let lightPurple = Color.init(.sRGB, red: 94/255, green: 29/255, blue: 235/255, opacity: 1)
+    static let gradient = Gradient(colors: [Color.darkPurple, Color.lightPurple])
+}
