4291af25b49830f5228db3fb9cb049fb52f96f3b
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2021-05-26 16:32:55 -0300

    deleting unnecessary files

---
+++ Multiplataform/Multiplataform/Views/SharedComponents/TabBar.swift
@@ -0,0 +1,20 @@
+//
+//  TabBar.swift
+//  Multiplataform
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 26/05/21.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
