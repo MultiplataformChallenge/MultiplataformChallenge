c1639590fa2b66ae02e18bdbecad2b58feaa5561
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2021-05-26 16:37:28 -0300

    when finish exercise go back to card screen

--- Multiplataform/watchOS WatchKit Extension/Views/ConfigExerciseView.swift
+++ Multiplataform/watchOS WatchKit Extension/Views/ConfigExerciseView.swift
@@ -9,50 +9,55 @@ import SwiftUI
 
 struct ConfigExerciseView: View {
     @ObservedObject var viewModel: AdjustExerciseViewModel
-
+    @Binding var rootIsActive: Bool
     var body: some View {
-        HStack {
-            VStack {
-                CircleButton(imageName: "pause", size: 55, fontSize: 30, action: { viewModel.pause() }, hasImage: true)
-                    .buttonStyle(BorderedButtonStyle(tint: .black))
-                Text("Pausar")
-                    .font(.caption2)
-                    .foregroundColor(.white)
-                    .offset(y: -8)
-            }
-            VStack {
-                CircleButton(imageName: "xmark", size: 55, fontSize: 30, action: { viewModel.finish() }, hasImage: true)
-                    .buttonStyle(BorderedButtonStyle(tint: .black))
-//                    .overlay(
-//                        Image(systemName: self.viewModel.finishActive ? "xmark" : "play")
-//                            .padding()
-//                            .frame(width: CGFloat(55), height: CGFloat(55))
-//                            .font(Font.system(size: CGFloat(30), weight: .black, design: .rounded))
-//                            .foregroundColor(.white)
-//                    )
-                Text("Encerrar")
-                    .font(.caption2)
-                    .foregroundColor(.white)
-                    .offset(y: -8)
-            }
-        } .offset(y: 16)
-        HStack {
-            VStack {
-                CircleButton(imageName: "backward.fill", size: 55, fontSize: 24, action: { viewModel.back()}, hasImage: true)
-                    .buttonStyle(BorderedButtonStyle(tint: .black))
-                Text("Anterior")
-                    .font(.caption2)
-                    .foregroundColor(.white)
-                    .offset(y: -8)
-            }
-            VStack {
-                CircleButton(imageName: "forward.fill", size: 55, fontSize: 24, action: { viewModel.foward()}, hasImage: true)
-                    .buttonStyle(BorderedButtonStyle(tint: .black))
-                Text("Próximo")
-                    .font(.caption2)
-                    .foregroundColor(.white)
-                    .offset(y: -8)
+        VStack(spacing: 8) {
+            HStack {
+                VStack {
+                    CircleButton(imageName: "pause", size: 55, fontSize: 30, action: { viewModel.pause() }, hasImage: true)
+                    //                    .buttonStyle(BorderedButtonStyle(tint: .black))
+                    Text("Pausar")
+                        .font(.caption2)
+                        .foregroundColor(.white)
+                        .offset(y: -8)
+                }
+                VStack {
+                    CircleButton(imageName: "xmark", size: 55, fontSize: 30, action: { viewModel.finish()
+                        self.rootIsActive = false
+                    }, hasImage: true)
+                    //                    .buttonStyle(BorderedButtonStyle(tint: .black))
+                    //                    .overlay(
+                    //                        Image(systemName: self.viewModel.finishActive ? "xmark" : "play")
+                    //                            .padding()
+                    //                            .frame(width: CGFloat(55), height: CGFloat(55))
+                    //                            .font(Font.system(size: CGFloat(30), weight: .black, design: .rounded))
+                    //                            .foregroundColor(.white)
+                    //                    )
+                    Text("Encerrar")
+                        .font(.caption2)
+                        .foregroundColor(.white)
+                        .offset(y: -8)
+                }
+            } .offset(y: 16)
+            HStack {
+                VStack {
+                    CircleButton(imageName: "backward.fill", size: 55, fontSize: 24, action: { viewModel.back()}, hasImage: true)
+                    //                    .buttonStyle(BorderedButtonStyle(tint: .black))
+                    Text("Anterior")
+                        .font(.caption2)
+                        .foregroundColor(.white)
+                        .offset(y: -8)
+                }
+                VStack {
+                    CircleButton(imageName: "forward.fill", size: 55, fontSize: 24, action: { viewModel.foward()}, hasImage: true)
+                    //                    .buttonStyle(BorderedButtonStyle(tint: .black))
+                    Text("Próximo")
+                        .font(.caption2)
+                        .foregroundColor(.white)
+                        .offset(y: -8)
+                }
             }
         }
     }
+    
 }
